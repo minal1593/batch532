c7d082efba8a1820a389b045b4fbcab3cc14d136
minal1593 <minalsalunkhe15@gmail.com>
2023-05-13 17:59:35 +0530

    Added : Basic program

---
+++ src/com/Bridgelabz/demo1.java
@@ -0,0 +1,19 @@
+package com.Bridgelabz;
+
+public class demo1
+{
+    public static void main(String[] args)
+    {
+        hello();
+        demo1 demo=new demo1();
+        demo.bye();
+    }
+    public static void hello()
+    {
+        System.out.println("Welcome");
+    }
+    public void bye()
+    {
+        System.out.println("good bye");
+    }
+}
---
+++ src/com/Bridgelabz/demo2.java
@@ -0,0 +1,13 @@
+package com.Bridgelabz;
+
+public class demo2
+{
+    public static void main(String[] args)
+    {
+        demo1.hello();
+        demo1 demo=new demo1();
+        demo.bye();
+
+    }
+
+}
